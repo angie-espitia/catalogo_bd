c9d15039363638c00033bd4fa393da3b563386f3
angie-espitia <angie-espitia@users.noreply.github.com>
2018-02-28 18:16:17 -0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -13,7 +13,7 @@ def lista(request):
 	return render(request, 'index.html')
 
 def mysql_db(request):
-	db = MySQLdb.connect(user='root', db='mysql', passwd='lucasmateo', host='localhost')
+	db = MySQLdb.connect(user='root', db='mysql', passwd='1234567', host='localhost')
 	cursor = db.cursor()
 	cursor.execute('SHOW databases')
 	nombres = [row[0] for row in cursor.fetchall()]
@@ -22,7 +22,7 @@ def mysql_db(request):
 	return render(request, 'mysql.html', {'nombres': nombres})
 
 def postgres_db(request):
-	db2 = psycopg2.connect(user='postgres', dbname='postgres', password='lucasmateo', host='localhost')
+	db2 = psycopg2.connect(user='postgres', dbname='postgres', password='1234567', host='localhost')
 	cursor = db2.cursor()
 	cursor.execute('select datname from pg_database')
 	nombres2 = [row[0] for row in cursor.fetchall()]
@@ -32,7 +32,7 @@ def postgres_db(request):
 
 
 def listar_tablas(request):
-	db = MySQLdb.connect(user='root', db='mysql', passwd='lucasmateo', host='localhost')
+	db = MySQLdb.connect(user='root', db='mysql', passwd='1234567', host='localhost')
 	cursor = db.cursor()
 	value = request.POST.get('value')
 	query = "show tables from %s" %value
@@ -45,7 +45,7 @@ def listar_tablas(request):
 
 def listar_tablas_campos_mysql(request):
 	dbs = request.POST.get('db')
-	db = MySQLdb.connect(user='root', db=dbs, passwd='lucasmateo', host='localhost')
+	db = MySQLdb.connect(user='root', db=dbs, passwd='1234567', host='localhost')
 	cursor = db.cursor()
 	value = request.POST.get('value')
 	query = "select * from %s" %value
@@ -61,7 +61,7 @@ def listar_tablas_campos_mysql(request):
 
 def listar_tablas_nombres_campos_mysql(request):
 	dbs = request.POST.get('db')
-	db = MySQLdb.connect(user='root', db=dbs, passwd='lucasmateo', host='localhost')
+	db = MySQLdb.connect(user='root', db=dbs, passwd='1234567', host='localhost')
 	cursor = db.cursor()
 	value = request.POST.get('value')
 	query = "DESCRIBE %s " % (value)
@@ -75,7 +75,7 @@ def listar_tablas_nombres_campos_mysql(request):
 def listar_tablas_postgres(request):
 	
 	value = request.POST.get('value')
-	db = psycopg2.connect(user='postgres', dbname=value, password='lucasmateo', host='localhost')
+	db = psycopg2.connect(user='postgres', dbname=value, password='1234567', host='localhost')
 	cursor = db.cursor()
 	query = "select table_name from information_schema.tables where table_schema = 'public'"
 	cursor.execute(query)
@@ -87,7 +87,7 @@ def listar_tablas_postgres(request):
 
 def listar_tablas_postgres_catalogo(request):
 	
-	db = psycopg2.connect(user='postgres', dbname='postgres', password='lucasmateo', host='localhost')
+	db = psycopg2.connect(user='postgres', dbname='postgres', password='1234567', host='localhost')
 	cursor = db.cursor()
 	query = "select table_name from information_schema.tables"
 	cursor.execute(query)
@@ -99,7 +99,7 @@ def listar_tablas_postgres_catalogo(request):
 
 def listar_tablas_campos_postgres(request):
 	dbs = request.POST.get('db')
-	db = psycopg2.connect(user='postgres', dbname=dbs, password='lucasmateo', host='localhost')
+	db = psycopg2.connect(user='postgres', dbname=dbs, password='1234567', host='localhost')
 	cursor = db.cursor()
 	value = request.POST.get('value')
 	query = "select * from %s" %value
@@ -115,7 +115,7 @@ def listar_tablas_campos_postgres(request):
 
 def listar_tablas_nombres_campos_postgres(request):
 	dbs = request.POST.get('db')
-	db = psycopg2.connect(user='postgres', dbname=dbs, password='lucasmateo', host='localhost')
+	db = psycopg2.connect(user='postgres', dbname=dbs, password='1234567', host='localhost')
 	cursor = db.cursor()
 	value = request.POST.get('value')
 	query = "select c.column_name FROM information_schema.columns c LEFT JOIN information_schema.element_types e ON c.table_catalog = e.object_catalog AND c.table_schema = e.object_schema AND c.table_name = e.object_name AND '%s' = e.object_type WHERE UPPER(c.table_name) = upper( '%s' ) ORDER BY c.ordinal_position" % (value,value)
